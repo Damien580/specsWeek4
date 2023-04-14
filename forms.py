14e357e1b55a2997d9cec7cdc22462f63112cbc5
Damien Redd <damiens.redd@gmail.com>
2023-04-13 19:19:09 -0500

    working on buttons

--- forms.py
+++ forms.py
@@ -16,3 +16,23 @@ class ProjectForm(FlaskForm): #creates class of forms for projects table, inheri
     def update_teams(self, teams): #takes in the self argument, as well as teams, to update a projects team info.
         self.team_selection.choices = [ (team.id, team.team_name) for team in teams ] #creates the selections for choosing a projects team(s).
     
+class ProjectUpdateForm(FlaskForm):
+    project_name = StringField('project name', validators=[DataRequired(), Length(min=4, max=255)]) #add an input for the project name.
+    description = TextAreaField('description') #adds an input field for a description.
+    completed = BooleanField('completed?') #adds a boolean check to see if completed.
+    team_selection = SelectField('team')
+    submit = SubmitField('submit')
+    
+    def re_update_teams(self, teams): #takes in the self argument, as well as teams, to update a projects team info.
+        self.team_selection.choices = [ (team.id, team.team_name) for team in teams ] #creates the selections for choosing a projects team(s).
+        
+class TeamUpdateForm(FlaskForm): #creates the class of forms for the teams table, inherits properties from FlaskForm.
+    team_name = StringField('team_name', validators=[DataRequired(), Length(min=4, max=255)])
+    submit = SubmitField("submit")      
+
+class DeleteForm(FlaskForm):
+    delete_item_select = SelectField('ID', choices = [])
+    submit = SubmitField('DELETE')
+    
+    def update_delete(self, projects):
+        self.delete_item_select.choices = [ (project.id, project.project_name) for project in projects ]
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 
-from forms import TeamForm, ProjectForm
+from forms import TeamForm, ProjectForm, ProjectUpdateForm, TeamUpdateForm, DeleteForm
 from model import db, User, Team, Project, connect_to_db
 
 app = Flask(__name__)
@@ -16,21 +16,6 @@ def home():
     project_form = ProjectForm()
     project_form.update_teams(User.query.get(user_id).teams)
     return render_template("home.html", team_form = team_form, project_form = project_form) #renders the html template, and inserts the TeamForm and ProjectForm forms.
-
-@app.route("/add-team", methods=["POST"]) #creates the add_team endpoint, as a post method.
-def add_team():
-    team_form = TeamForm() #assigns TeamForm to a variable.
-    
-    if team_form.validate_on_submit(): #checks that all form validators are met.
-        team_name = team_form.team_name.data
-        new_team = Team(team_name, user_id)
-        with app.app_context():
-            db.session.add(new_team)
-            db.session.commit()
-        return redirect(url_for("home")) #redirects to home page
-    else:
-        return redirect(url_for("home"))
-    
 
 
 @app.route("/add-project", methods=["POST"])
@@ -51,22 +36,86 @@ def add_project():
     else:
         return redirect(url_for("home"))
     
+@app.route("/all-projects")
+def all_projects():
+    project_list = Project.query.all()
+    project_update_form = ProjectUpdateForm()
+    project_update_form.re_update_teams(User.query.get(user_id).teams)
+    project_delete_form = DeleteForm()
+    project_delete_form.update_delete(Project.query.all())
+    return render_template("all_projects.html", project_list = project_list, project_update_form = project_update_form, project_delete_form = project_delete_form)
 
+@app.route("/update-project", methods=["POST"])
+def update_project():
+    project_update_form = ProjectUpdateForm()
+    project_update_form.re_update_teams(User.query.get(user_id).teams)
+    
+    if project_update_form.validate_on_submit():
+        project_name = project_update_form.project_name.data
+        description = project_update_form.description.data
+        completed = project_update_form.completed.data
+        team_selection = project_update_form.team_selection.data
+        updated_project = Project(project_name, description, completed, team_selection)
+        with app.app_context():
+            db.session.add(updated_project)
+            db.session.commit()
+        return redirect(url_for("all_projects"))
+    else:
+        return redirect(url_for("all_projects"))
+
+@app.route("/add-team", methods=["POST"]) #creates the add_team endpoint, as a post method.
+def add_team():
+    team_form = TeamForm() #assigns TeamForm to a variable.
+    
+    if team_form.validate_on_submit(): #checks that all form validators are met.
+        team_name = team_form.team_name.data
+        new_team = Team(team_name, user_id)
+        with app.app_context():
+            db.session.add(new_team)
+            db.session.commit()
+        return redirect(url_for("home")) #redirects to home page
+    else:
+        return redirect(url_for("home"))
 
 @app.route("/all-teams")
 def all_teams():
     team_list = Team.query.filter_by(user_id = user_id).all()
+    team_update_form = TeamUpdateForm()
     
-    return render_template("all_teams.html", team_list = team_list)
+    return render_template("all_teams.html", team_list = team_list, team_update_form = team_update_form)
     
-@app.route("/all-projects")
-def all_projects():
-    project_list = Project.query.all()
+@app.route("/update-team", methods=["POST"])
+def update_team():
+    team_update_form = TeamUpdateForm()
     
-    return render_template("all_projects.html", project_list = project_list)
+    if team_update_form.validate_on_submit(): #checks that all form validators are met.
+        team_name = team_update_form.team_name.data
+        new_team = Team(team_name, user_id)
+        with app.app_context():
+            db.session.add(new_team)
+            db.session.commit()
+        return redirect(url_for("all_teams")) 
+    else:
+        return redirect(url_for("all_teams"))
+    
+@app.route("/project-delete", methods=["POST"])
+def project_delete():
+    project_delete_form = DeleteForm()
+    project_delete_form.update_delete(Project.query.all())
+    
+    if project_delete_form.validate_on_submit():
+        project_id = project_delete_form.delete_item_select.data
+        print("************************deleting item,", type(project_id)) 
+        new_delete = Project.query.filter_by(id = project_id).all()
+        print(new_delete[0].id)
+        with app.app_context():
+            db.session.delete(new_delete[0])
+            db.session.commit()
+        print("===================================")
+        return redirect(url_for("all_projects"))
+    else:
+        return redirect(url_for("all_projects"))
 
-
-    
 
 
 
@@ -74,3 +123,5 @@ def all_projects():
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(debug = True, port = 8000, host = "localhost")
+
+
